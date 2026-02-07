ebb72b2bd4688662621afe17dae90f99cc382a79
ishrak79 <rumaiyasultana2@gmail.com>
2026-02-07 21:46:28 +0600

    hi

---
+++ Conditional Statement and lists/conditionals.py
@@ -0,0 +1,8 @@
+number = int(input("Enter Number to check"))
+print(f"Number to be checked: {number}")
+
+if number%2==0:
+    print("THis is an even number")
+else:
+    print("This is an odd number")
+print("Out of conditional statemen")
---
+++ Python2/operators.py
@@ -0,0 +1,8 @@
+num1 = 45
+num2 = 3
+print("Number 1", num1)
+print("Number 2", num2)
+print("Addition :",num1+num2)
+print("Different :", num1-num2)
+print("product :", num1*num2)
+print("Division :")
